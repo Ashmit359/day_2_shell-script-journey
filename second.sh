aed36138c6fb9624d7d4d72bc259a8f1b61d572d
Ashmit359 <ashmit.sinha359@gmail.com>
2024-10-22 19:52:20 +0530

    Update second.sh with new content

--- second.sh
+++ second.sh
@@ -1,3 +1,97 @@
 #!/usr/bin/python3
 
 print("hello ashmit sinha")
+
+
+#this is all command to i am learn today
+# ┌──(ashmit359㉿DESKTOP-73I0NVD)-[~]
+# └─$ ls
+# shellscript-learning
+# 
+# ┌──(ashmit359㉿DESKTOP-73I0NVD)-[~]
+# └─$ cd shellscript-learning/
+# 
+# ┌──(ashmit359㉿DESKTOP-73I0NVD)-[~/shellscript-learning]
+# └─$ ls
+# first.sh
+# 
+# ┌──(ashmit359㉿DESKTOP-73I0NVD)-[~/shellscript-learning]
+# └─$ vi first.sh
+# 
+# ┌──(ashmit359㉿DESKTOP-73I0NVD)-[~/shellscript-learning]
+# └─$ vi first.sh
+# 
+# ┌──(ashmit359㉿DESKTOP-73I0NVD)-[~/shellscript-learning]
+# └─$ ./first.sh
+# this is my first shellscript
+# my name is ashmit sinha
+# first.sh
+# /home/ashmit359/shellscript-learning
+# 
+# ┌──(ashmit359㉿DESKTOP-73I0NVD)-[~/shellscript-learning]
+# └─$ vi first.sh
+# 
+# ┌──(ashmit359㉿DESKTOP-73I0NVD)-[~/shellscript-learning]
+# └─$ ./first.sh
+# this is my first shellscript
+# my name is ashmit sinha
+# first.sh
+# /home/ashmit359/shellscript-learning
+# 
+# ┌──(ashmit359㉿DESKTOP-73I0NVD)-[~/shellscript-learning]
+# └─$ ps -ef
+# UID        PID  PPID  C STIME TTY          TIME CMD
+# root         1     0  0 18:03 hvc0     00:00:00 /init
+# root         7     1  0 18:03 hvc0     00:00:00 plan9 --control-socket 5 --log-level 4 --server-fd 6 --pipe-fd 8 --log-truncate
+# root        10     1  0 18:03 ?        00:00:00 /init
+# Vroot        11    10  0 18:03 ?        00:00:00 /init
+# ashmit3+    12    11  0 18:03 pts/0    00:00:00 -bash
+# ashmit3+    43    12  0 18:14 pts/0    00:00:00 ps -ef
+# 
+# ┌──(ashmit359㉿DESKTOP-73I0NVD)-[~/shellscript-learning]
+# └─$ which python3
+# /usr/bin/python3
+# 
+# ┌──(ashmit359㉿DESKTOP-73I0NVD)-[~/shellscript-learning]
+# └─$ vi second.sh
+# 
+# ┌──(ashmit359㉿DESKTOP-73I0NVD)-[~/shellscript-learning]
+# └─$ ./second.sh
+# -bash: ./second.sh: Permission denied
+# 
+# ┌──(ashmit359㉿DESKTOP-73I0NVD)-[~/shellscript-learning]
+# └─$ chmod +x second.sh
+# 
+# ┌──(ashmit359㉿DESKTOP-73I0NVD)-[~/shellscript-learning]
+# └─$ ./second.sh
+# hello ashmit sinha
+# 
+# ┌──(ashmit359㉿DESKTOP-73I0NVD)-[~/shellscript-learning]
+# └─$ cat first.sh
+# #!/bin/bash
+# echo "this is my first shellscript"
+# echo "my name is ashmit sinha"
+# 
+# ls
+# pwd
+# sleep 1
+# 
+# ┌──(ashmit359㉿DESKTOP-73I0NVD)-[~/shellscript-learning]
+# └─$ cat second.sh
+# #!/usr/bin/python3
+# print("hello ashmit sinha")
+# 
+# ┌──(ashmit359㉿DESKTOP-73I0NVD)-[~/shellscript-learning]
+# └─$ ./second.sh
+# hello ashmit sinha
+# 
+# ┌──(ashmit359㉿DESKTOP-73I0NVD)-[~/shellscript-learning]
+# └─$ vi day_2_sell_script-journey
+# 
+# ┌──(ashmit359㉿DESKTOP-73I0NVD)-[~/shellscript-learning]
+# └─$ ./second.sh
+# hello ashmit sinha
+# 
+# ┌──(ashmit359㉿DESKTOP-73I0NVD)-[~/shellscript-learning]
+# └─$ vi second.sh
+# 
